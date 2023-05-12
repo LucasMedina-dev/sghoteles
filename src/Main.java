4a629900c1e9487729dac35544a6e52feb59626c
lucas <medinalucasian@gmail.com>
2023-05-12 16:48:50 -0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+
+/**
+ *
+ * @author Lucas
+ */
+
+import com.login.Login;
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        Login login= new Login();
+        login.setVisible(true);
+    }
+}
