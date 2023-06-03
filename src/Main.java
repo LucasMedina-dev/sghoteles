26da8951217e9ddb30d61154e2eadbb6ef3a860e
MauLeoz <mauricio.leoz.89@gmail.com>
2023-06-03 19:19:12 -0300

    modificado

--- src/Main.java
+++ src/Main.java
@@ -22,51 +22,6 @@ public class Main {
         h.add(h2);
         h.add(h3);
 
-        /*
-        Hotel hotel = new Hotel(h);
-
-        System.out.println("cual es su dni?");
-        String dni = scanner.nextLine();
-
-        Cliente clienteBuscado = miHashMap.get(dni);
-
-        if (clienteBuscado != null) {
-            System.out.println("Cliente encontrado: " + clienteBuscado.getNombre());
-        } else {
-            System.out.println("Cliente no encontrado");
-        }
-
-        hotel.mostrarHabitacionesLibres();
-
-
-        System.out.println("seleccione una habitacion");
-        int num = scanner.nextInt();
-
-
-        Habitacion habitacionBuscada = null;
-        for (Habitacion habitacion : h) {
-            if (habitacion.getNumero() == num) {
-                habitacionBuscada = habitacion;
-                break;
-            }
-        }
-
-        if (habitacionBuscada != null) {
-            System.out.println("Habitación encontrada: " + habitacionBuscada);
-        } else {
-            System.out.println("Habitación no encontrada");
-        }
-
-        System.out.println("cuantos dias se quedara?");
-        int dias = scanner.nextInt();
-
-        Estadia estadia = new Estadia(1,dias,clienteBuscado,habitacionBuscada);
-
-        Factura factura = new Factura(1,estadia);
-
-         */
-
-
         System.out.println();
         mostrarHabitacionesOcupadas(h);
 
